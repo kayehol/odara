7da53dfc1e1d30b9ee6a3f9bd622553cf7bf2758
kayehol <kaye.holanda@gmail.com>
2020-04-15 22:05:23 -0300

    Construção da Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,41 @@
 import React from "react"
 import styled from "styled-components"
 import Layout from "../components/layout"
+import logo from "./logoPrincipal.png";
+import padrao from "./padrao1.png"
 
 const HeaderWrapper = styled.div`
     background-color: #000000;
     color: #ffffff;
     font-size: 0.9em;
-    padding: 10px;
+    padding: 0;
+    margin: 0;
     ul {
         list-style-type: none;
         display: flex;
         flex-direction: row;
         justify-content: center;
         align-items: center;
-        margin: 20px;
+        margin: 0px;
         padding: 0;
     }
-
     ul li {
         margin-right: 50px;
+        height: 55px;
+        display: flex;
+        align-items: center;
+        padding: 10px;
     }
-    
     ul a:link {
         text-decoration: none;
         color: #fff;
-      }
-      
+    }
     ul a:visited {
         text-decoration: none;
-      }
-      
+    }
+    ul li:hover {
+        background-color: #c03238;
+    }
 `
 
 const Header = () => (
@@ -45,9 +51,31 @@ const Header = () => (
     </HeaderWrapper>
 )
 
+const HomeWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+    justify-content: center;
+    #logo {
+        margin: 25px 0 25px 0;
+    }
+    #padrao{
+        width: 100%;
+        margin-top: 20px;
+    }
+`
+
+const Home = () => (
+    <HomeWrapper>
+        <img src={padrao} alt="Odara" id="padrao"/>
+        <img src={logo} alt="Logo Odara" width="50%" id="logo" />
+        <img src={padrao} alt="Odara" id="padrao"/>
+    </HomeWrapper>
+)
+
 export default () => (
     <Layout>
         <Header />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <Home />
     </Layout>
 )
